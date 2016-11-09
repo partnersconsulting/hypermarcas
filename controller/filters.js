3af5272a588d45f55b832de49cd2d37336f9c030
SheldonChaves <sheldonchaves@gmail.com>
2016-11-09 00:00:17 -0200

    r

--- controller/filters.js
+++
@@ -1,6 +0,0 @@
-angular.module("App.filters", [])
-    .filter('interpolate', function(version) {
-        return function(text) {
-            return String(text).replace(/\%VERSION\%/mg, version);
-        }
-    });
--- controller/navbar-controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('App')
-    .controller('NavBarController', function($scope) {
-
-        $scope.isCollapsed = true;
-
-        $scope.toggle = function() {
-            $scope.isCollapsed = !$scope.isCollapsed
-        }
-    })
